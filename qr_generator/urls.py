1b8ec8dcebde6d5cc20175449a4eeed72b8b2803
Asadaugai <asadaugai@gmail.com>
2025-11-20 16:59:29 +0500

    secure the url

--- qr_generator/urls.py
+++ qr_generator/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('generate/url/', views.generate_url_qr, name='generate_url_qr'),
     path('generate/file/', views.generate_file_qr, name='generate_file_qr'),
     path('result/<int:qr_id>/', views.qr_result, name='qr_result'),
-    path('download/<uuid:token>/', views.download_file, name='download_file'),
+    path('<uuid:token>/', views.download_file, name='download_file'),
 ]
